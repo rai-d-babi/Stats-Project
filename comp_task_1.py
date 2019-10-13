82e4dd892fe81243194be9e1d1247c9482d60f9a
Babi Rai <babi.rai@hotmail.com>
2019-10-13 16:28:55 +0100

    Stats comp task

--- comp_task_1.py
+++ comp_task_1.py
@@ -66,20 +66,73 @@ for i in range(len(n)):
     for j in range(len(theta)):
         post[i].append(0.5*(fact(n[i])*theta[j]**k[i]*(1-theta[j])**(n[i]-k[i]))/float(fact(n[i]-k[i])))
 
+# normalise the densities
+
+# list filled with zeros for the total sum of posterior probabilities for each experiment
+tot_sum = zeros(4)
+for i in range(len(n)):
+    for j in range(len(theta)):
+        tot_sum[i]+=post[i][j]
+
+dx = theta[1]-theta[0]
+
+# empty list for filling area of unnormalised densities
+coeff = []
+# empty list for filling normalised densities
+post_n = []
+for i in range(len(n)):
+    coeff.append(tot_sum[i]*dx)
+    post_n.append	(post[i]/coeff[i])
+    print(sum(post_n[i]*dx))
+
+
 # convert list object as numpy array
 post = np.asarray(post)
+post_n = np.asarray(post_n)
 
 # Sub-plots of posterior of different experiments
 
 fig, axs = plt.subplots(2, 2)
 fig.suptitle('Posterior')
-axs[0, 0].plot(theta, post[0], 'r-o')
+# string that are labels 
+n_str = ['n = 1', 'n = 10', 'n = 50', 'n = 100']
+# y-coordinate
+
+for i in range(2):
+    axs[0,i].text(0.6,0.75,n_str[i], fontsize = 12,transform = axs[0,i].transAxes)
+
+axs[0, 0].plot(theta, post[0],  'r-o')
 axs[0, 1].plot(theta, post[1], 'g-o')
+for i in range(2):
+    axs[1,i].text(0.6, 0.75,n_str[2+i], fontsize = 12,transform = axs[1,i].transAxes)
+
 axs[1, 0].plot(theta, post[2], 'b-o')
 axs[1, 1].plot(theta, post[3], 'k-o')
 
 for ax in axs.flat:
-    ax.set(xlabel=r'$\theta$', ylabel=r'p($\theta$|D)')
+    ax.set(xlabel=r'$\theta$', ylabel=r'Density p($\theta$|D)')
 
 plt.savefig(r'Posterior vs theta')
+
+# Sub-plots of normalised posterior of different experiments
+fig, axs1 = plt.subplots(2, 2)
+fig.suptitle('Normalised Posterior')
+
+
+for i in range(2):
+    axs1[0,i].text(0.6,0.75,n_str[i], fontsize = 12,transform = axs1[0,i].transAxes)
+
+axs1[0, 0].plot(theta, post_n[0],  'r-o')
+axs1[0, 1].plot(theta, post_n[1], 'g-o')
+for i in range(2):
+    axs1[1,i].text(0.6, 0.75,n_str[2+i], fontsize = 12,transform = axs1[1,i].transAxes)
+
+axs1[1, 0].plot(theta, post_n[2], 'b-o')
+axs1[1, 1].plot(theta, post_n[3], 'k-o')
+
+for ax in axs1.flat:
+    ax.set(xlabel=r'$\theta$', ylabel=r'Normalised Density p($\theta$|D)/c')
+
+print(post[3], post_n[3])
+plt.savefig(r'Normalised Posterior vs theta')
 plt.show()
